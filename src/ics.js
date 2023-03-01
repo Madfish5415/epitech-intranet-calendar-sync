174a825a2187eacfecf3dca7cff85f3f25daa169
Lucas Mathieux <lucas.mathieux@epitech.eu>
2023-03-01 11:29:32 +0100

    fix: remove utc function

--- src/ics.js
+++ src/ics.js
@@ -6,8 +6,8 @@ const convertToICS = (event) => {
         title: event.acti_title,
         location: event.room?.code.split("/")[3] || "Unknown",
         url: "https://intra.epitech.eu/module/" + event.scolaryear + "/" + event.codemodule + "/" + event.codeinstance + "/" + event.codeacti,
-        start: moment.tz(event.start, process.env.TZ || "Europe/Paris").utc().format('YYYY-M-D-H-m').split("-").map(val => parseInt(val)),
-        end: moment.tz(event.end, process.env.TZ || "Europe/Paris").utc().format('YYYY-M-D-H-m').split("-").map(val => parseInt(val)),
+        start: moment.tz(event.start, process.env.TZ || "Europe/Paris").format('YYYY-M-D-H-m').split("-").map(val => parseInt(val)),
+        end: moment.tz(event.end, process.env.TZ || "Europe/Paris").format('YYYY-M-D-H-m').split("-").map(val => parseInt(val)),
         status: 'CONFIRMED',
         busyStatus: 'BUSY',
     }
